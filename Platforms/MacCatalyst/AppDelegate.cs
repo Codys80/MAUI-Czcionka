01f08b702250e1c0b45b61eae49eebbea222520c
CodyS80 <bartolosemolo@gmail.com>
2025-10-06 09:28:20 +0200

    Dodanie projektu

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUI_Czcionka
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
